4ed1315287d795311e2d4f2ebe4a70cd45a2c186
Doreina <barkauskaite.dovile@gmail.com>
2020-09-23 23:04:08 +0300

    Create new project

---
+++ src/main/java/lt/dovile/warehouseinfosystem/RunConsole.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.dovile.warehouseinfosystem;
+
+/**
+ *
+ * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
+ */
+public class RunConsole {
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        System.out.println("Hello");
+    }
+}
