9afbe184c0de97fe4ecb255f9a24252d54a30120
Doreina <barkauskaite.dovile@gmail.com>
2020-09-24 00:18:54 +0300

    Create class and interface for action, which is into request. There are two methods: filter by quantity and filter by date

---
+++ src/main/java/lt/dovile/warehouseinfosystem/service/ActionService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.dovile.warehouseinfosystem.service;
+
+
+import lt.dovile.warehouseinfosystem.data.Item;
+import java.util.List;
+
+/**
+ *
+ * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
+ */
+public interface ActionService {
+    /**
+     * 
+     * @param quantity
+     * @return New Items list which quantities are less than request quantity 
+     */
+    List<Item> getLessItemsByQuantity(String requestQuantity);
+    
+    /**
+     * 
+     * @param date
+     * @return New Items list which expiration Date are oldest than request date
+     */
+    List<Item> getLessItemsByDate(String requestDate);
+}
---
+++ src/main/java/lt/dovile/warehouseinfosystem/service/ActionServiceImpl.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.dovile.warehouseinfosystem.service;
+
+
+import lt.dovile.warehouseinfosystem.data.Item;
+import lt.dovile.warehouseinfosystem.readfile.DataReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
+ */
+public class ActionServiceImpl implements ActionService {
+
+    private static final List<Item> FULL_ITEMS_LIST = new DataReader().readStockFile();
+
+    public List<Item> requestItemsList = new ArrayList<>();
+/**
+ * 
+ * @param requestQuantity
+ * @return new Items list when items quantities are less than request quantity. 
+ * If request is error or there isn't any items with this request return empty list.
+ */
+    public List<Item> getLessItemsByQuantity(String requestQuantity) {
+        int quantity;
+        try {
+            quantity = Integer.parseInt(requestQuantity);
+        } catch (Exception ex) {
+            return requestItemsList;
+        }
+        for (Item item : FULL_ITEMS_LIST) {
+            if (item.getQuantity() < quantity) {
+                requestItemsList.add(item);
+            }
+        }
+        return requestItemsList;
+    }
+/**
+ * 
+ * @param requestDate
+ * @return return New Items list which expiration Date are oldest than request date.
+ * If request is error or there isn't any items with this request return empty list.
+ */
+    public List<Item> getLessItemsByDate(String requestDate) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date;
+        try {
+            date = sdf.parse(requestDate);
+        } catch (ParseException ex) {
+            return requestItemsList;
+        }
+        for (Item item : FULL_ITEMS_LIST) {
+            if (item.getExpirationDate().compareTo(date) < 0) {
+                requestItemsList.add(item);
+            }
+        }
+        return requestItemsList;
+    }
+}
