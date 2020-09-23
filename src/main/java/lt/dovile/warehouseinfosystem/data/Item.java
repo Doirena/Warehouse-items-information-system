fa739ed07c21dadac49c103531b259008666e26c
Doreina <barkauskaite.dovile@gmail.com>
2020-09-23 23:07:02 +0300

    Create item class where is item's parameters

---
+++ src/main/java/lt/dovile/warehouseinfosystem/data/Item.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dovile.data;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
+ */
+public class Item {
+
+    public String name;
+    private long code;
+    private int quantity;
+    private Date expirationDate;
+
+    public Item() {
+    }
+
+    public Item(String name, long code, int quantity, Date expirationDate) {
+        this.name = name;
+        this.code = code;
+        this.quantity = quantity;
+        this.expirationDate = expirationDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Override
+    public String toString() {
+        return name + " " + code + " " + quantity + " " + sdf.format(expirationDate);
+    }
+
+}
